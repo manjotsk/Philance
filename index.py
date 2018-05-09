532f662f2e2f22a8b23cbe6f7c8d3abe3c92ddbe
RamSailopal <thelinuxguys@yahoo.com>
2018-05-09 14:50:01 +0100

    Adding php landing page

---
+++ index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+print "Content-type:text/html";
+print "\r\n";
+print "<html>";
+print "<head>";
+print "<body>";
+print "<p><h1>Philance Landing Page</h1></p>";
+print "</body>";
+print "</head>";
+print "</html>";
