c8addc38eeec484d395a8bc4eb2ab649d1f4e3b8
RamSailopal <thelinuxguys@yahoo.com>
2018-05-11 08:53:55 +0100

    Testing carriage return line feeds

--- index1.py
+++ index1.py
@@ -4,7 +4,7 @@ print "\r\n";
 print "<html>";
 print "<head>";
 print "<body>";
-print "<p><h1>Philance Landing Page</h1></p>";
+print "<p><h1>Philance Landing Page2</h1></p>";
 print "</body>";
 print "</head>";
 print "</html>";
