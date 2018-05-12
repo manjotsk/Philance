c9147898a900fe10e6c79d703e6d9492fd60cc8b
RamSailopal <thelinuxguys@yahoo.com>
2018-05-12 17:34:15 +0100

    Just testing these bloomin carriage return and  line feeds

---
+++ ramtest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print "Content-type:text/html";
+print "\r\n";
+print "<html>";
+print "<head>";
+print "<body>";
+print "<p><h1>Philance Landing Page2</h1></p>";
+print "<p><h2>Ram testing</h2></p>";
+print "</body>";
+print "</head>";
+print "</html>";
