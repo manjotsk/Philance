361f57d8e2aa07002d2d1a067c41f76fa35d33ea
RamSailopal <thelinuxguys@yahoo.com>
2018-05-17 15:52:49 +0100

    Added overview comment to script

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#
+#	AUTHOR - Raman Sailopal
+#
+#	Automated script to allow remote access to local mysql instance
+#
 pass=$(awk '/root_password/ { print $2 }' /etc/openproject/installer.dat)
 mysql --user=root --password=$pass -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
