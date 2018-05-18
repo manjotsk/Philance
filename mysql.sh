84c9f63eebae59f994cc4a44c6e4194922951b9d
RamSailopal <thelinuxguys@yahoo.com>
2018-05-18 11:30:14 +0100

    Added code to check for mysql database and import it.

--- mysql.sh
+++ mysql.sh
@@ -6,3 +6,7 @@
 #
 pass=$(awk '/root_password/ { print $2 }' /etc/openproject/installer.dat)
 mysql --user=root --password=$pass -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+if test -f /opt/openproject/public/Philance/mysql.sql
+then
+      mysql --user=root --password=$pass < /opt/openproject/public/Philance/mysql.sql
+fi
