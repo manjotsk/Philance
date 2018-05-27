a64bc7e13683d29357c02a4e3c56c7b4f168c6d6
RamSailopal <thelinuxguys@yahoo.com>
2018-05-27 16:39:22 +0100

    Additional libraries as requested by Ajay for code linting

--- devlibraries.sh
+++ devlibraries.sh
@@ -8,7 +8,17 @@
 #
 #	If any libraries are built on machines please update this file also
 #
+cp /opt/openproject/public/Philance/my-app/.eslintrc /opt/openproject/public/philance-app/
+cp /opt/openproject/public/Philance/my-app/package.json /opt/openproject/public/philance-app/
 cd /opt/openproject/public/philance-app
-npm install -g express --save
+sudo npm install -g express --save
 cd /tmp;wget https://dev.mysql.com/get/Downloads/Connector-Nodejs/mysql-connector-nodejs-8.0.11.tar.gz
-cd /opt/openproject/public/philance-app;sudo npm install -g /tmp/mysql-connector-nodejs-8.0.11.tar.gz
+cd /opt/openproject/public/philance-app
+sudo npm install -g /tmp/mysql-connector-nodejs-8.0.11.tar.gz
+sudo npm install -g --save-dev eslint
+sudo npm install -g --save-dev eslint-config-airbnb
+sudo npm install -g --save-dev eslint-plugin-import
+sudo npm install -g --save-dev eslint-plugin-jsx-a11y
+sudo npm install -g --save-dev eslint-plugin-react
+sudo npm install -g --save-dev babel-eslint
+sudo npm install -g --save-dev pre-commit
