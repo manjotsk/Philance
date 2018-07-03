845e1c176aea9b068e6da1eb3d1174c7c61c340d
Raman Sailopal <38779448+RamSailopal@users.noreply.github.com>
2018-07-03 10:36:07 +0100

    Changes to allow remote access to philance db on dev environments. Also added entries to ignore node_modules directories in gitignore. (#61)

--- mysql.sh
+++ mysql.sh
@@ -10,7 +10,7 @@ if [[ "$?" == "1" ]]
 then
 	mysql --user=root --password=$pass < /opt/openproject/public/Philance/SQL_Tables.sql
 	mysql --user=root --password=$pass < /opt/openproject/public/Philance/SQL_Insert.sql
-	mysql --user=root --password=$pass -e "GRANT ALL PRIVILEGES ON philance.* TO 'philance'@'localhost' IDENTIFIED BY 'ph1ldb';"
+	mysql --user=root --password=$pass -e "GRANT ALL PRIVILEGES ON philance.* TO 'philance'@'%' IDENTIFIED BY 'ph1ldb' WITH GRANT OPTION;"
 fi
 mysql --user=root --password=$pass -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
 if test -f /opt/openproject/public/Philance/mysql.sql
