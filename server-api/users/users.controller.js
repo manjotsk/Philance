1f9636c187b3b537208064e807790130de4f6564
manjot <manjot.simbacart@gmail.com>
2018-07-26 05:57:21 +0530

    Added location filter to selected users. Changed Get method to Post.

--- server-api/users/users.controller.js
+++ server-api/users/users.controller.js
@@ -4,9 +4,9 @@ const jwt = require("jsonwebtoken");
 var moment = require('moment')
 var users = require("./users.model");
 const sequelize = require('../util/dbconnection');
-
+var helper = require('../helpers/common');
+var commonFunctions = helper.commonFunctions;
 exports.register = (req, res, next) => {
-
     sequelize.sync().then(() => users.findOne({ where: { email: req.body.email } }).then(_user => {
         console.log('_user : ' + _user);
         if (_user == null) {
@@ -16,16 +16,17 @@ exports.register = (req, res, next) => {
                     return res.status(500).json({
                         error: "password hashing failed! detailed error as follows - " + err
                     });
-                } else {                    
+                } else {
                     users.create({
                         firstName: req.body.firstName,
                         lastName: req.body.lastName,
                         email: req.body.email,
-                        password: hash
+                        password: hash,
+                        location: req.body.location
                     }).then((_user) => {
                         res.status(200).json({
                             message: "User # " + req.body.firstName + " " + req.body.lastName + " Registered Successfully",
-                            user : _user
+                            user: _user
                         });
                     })
                 }
@@ -34,7 +35,7 @@ exports.register = (req, res, next) => {
             console.log('User Registered');
             return res.status(409).json({
                 message: "User # " + req.body.firstName + " " + req.body.lastName + " already registered",
-                user : _user
+                user: _user
             })
         }
 
@@ -81,35 +82,77 @@ exports.login = (req, res, next) => {
     )
 
 }
-    
+
 exports.search = (req, res, next) => {
     //TODO: Add Validators
     var userName;
-    if(Object.keys(req.query).length === 0){
-        var searchQuery = 'SELECT * from users';
-        connectionPool.query(searchQuery, (err, users) => {
-            if (err) {
-                res.status(500).send('Error with the database')
-                return;
-            } else {
-                res.status(200).send(users);
+
+    if (Object.keys(req.body).length === 0) {                //if no seach body parameter is provided
+        sequelize.sync()
+            .then(() => {
+                users.findAll({
+                    attributes: ['user_id', 'location']
+                })
+                    .then(_users => {
+                        res.status(200).send(_users)
+                    })
+            })
+
+    } else {                                                  //if seach body parameter is provided
+        var firstName = req.body.fname;
+        var personType = req.body.pType;
+        var lastName = req.body.lname;
+        var location = req.body.loc;
+        var distance = req.body.dist;
+
+        if (firstName && lastName) {
+            //if only first name/ last name is provided
+            if ((firstName && lastName) && (personType == null && distance == null && location == null)) {
+                var searchQuery = 'SELECT * FROM users WHERE fname LIKE ? AND lname LIKE ?';
+                connectionPool.query(searchQuery, [firstName, lastName], (err, users) => {
+                    if (err) {
+                        res.status(500).send('Error with the database' + err)
+                        return;
+                    } else {
+                        res.status(200).send(users);
+                    }
+                })
             }
-        })
-    }else{
-        var firstName=req.query.fname;
-        var personType=req.query.pType;
-        var lastName=req.query.lname;
-        var location=req.query.loc;
-        var distance=req.query.dist;
-        var searchQuery = 'SELECT * FROM users WHERE fname LIKE ? AND lname LIKE ?';
-        connectionPool.query(searchQuery,[firstName,lastName],(err,users)=>{
-            if (err) {
-                res.status(500).send('Error with the database'+err)
-                return;
-            } else {
-                res.status(200).send(users);
+            //if distance/location is provided    
+
+
+        }
+        else {
+            if (((distance || location))) {
+                if (distance == null) {
+                    return res.status(409).send('please provide distance')
+                }
+                if (location == null) {
+                    return res.status(409).send('please provide a location')
+                }
+
+                sequelize.sync()
+                    .then(() => {
+                        users.findAll({
+                            attributes: ['user_id', 'location']
+                        })
+                            .then(_users => {
+                                commonFunctions.entitiesDistanceValidator(_users, req, (error, response) => {
+                                    if (error) {
+                                        res.status(500).send(error)
+                                    } else {
+                                        res.status(200).send(response)
+                                    }
+                                    return;
+                                })
+                            })
+                    })
+
             }
-        })
+
+        }
+        //TODO if firstname/ Last name is provided with distance
+
         // res.status(200).send(req.query);
     }
 }
