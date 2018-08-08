3fe7d97ed2908303c4e7915e24f6b71f2c40bce0
RamSailopal <thelinuxguys@yahoo.com>
2018-08-08 11:10:15 +0100

    More verbosity on Selenium run script. Also added a Windows batch file so runtest can be run off a short cut.

--- frontend-test/runtests.sh
+++ frontend-test/runtests.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#
+#	AUTHOR - Raman Sailopal
+#
+#	Script to run Selenium front end, Reactjs tests through nosetests 
+#	for html reporting. 
+#
 export PATH=$PATH:/home
 export DISPLAY=:1
 if [[ "$1" == "" ]]
 then
-	nosetests *.py --with-html --html-report=test.html
+	echo "Running ALL tests ..."
+	echo
+	echo
+	nosetests /opt/openproject/public/Philance/frontend-test/*.py --with-html --html-report=/opt/openproject/public/Philance/frontend-test/test.html
 else
-	nosetests $1 --with-html --html-report=test.html
+	echo "Running tests for $1"
+	echo
+	echo
+	nosetests /opt/openproject/public/Philance/frontend-test/$1 --with-html --html-report=/opt/openproject/public/Philance/frontend-test/test.html
 fi
