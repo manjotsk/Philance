bf426d70bd1f547bafbbe530ca0a4758881936b8
Raman Sailopal <38779448+RamSailopal@users.noreply.github.com>
2018-08-22 15:48:47 +0100

    Added rudimentary home page selenium tests (#97)

---
+++ frontend-test/findaproject1.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+	options.add_argument('--headless')
+	options.add_argument('--disable-gpu')
+	options.add_argument('--window-size=1500,1000')
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:3000/home")
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Start A Project'])[1]/following::div[1]").click()
+	driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Distance From You'])[1]/following::div[3]").click()
+	try: self.assertEqual("Within 25 miles", driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Within 10 miles'])[1]/following::li[1]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Philance, Inc.'])[1]/following::div[3]").click()
+        time.sleep(5)
+	driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Skills Needed'])[1]/following::div[3]").click()
+        time.sleep(5)
+	try: self.assertEqual("IT", driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Choose Skills Needed'])[1]/following::li[1]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Philance, Inc.'])[1]/following::div[3]").click()
+	time.sleep(5)
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Impact Category'])[1]/following::div[3]").click()
+        time.sleep(5)
+	try: self.assertEqual("Elderly", driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Community'])[1]/following::li[1]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Philance, Inc.'])[1]/following::div[3]").click()
+	time.sleep(5)
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Resource Type'])[1]/following::div[3]").click()
+        time.sleep(5)
+	try: self.assertEqual("Needs Volunteers", driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Any'])[4]/following::li[1]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Philance, Inc.'])[1]/following::div[3]").click()
+	time.sleep(5)
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Project Status'])[1]/following::div[3]").click()
+        time.sleep(5)
+	try: self.assertEqual("Closed", driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Active'])[1]/following::li[1]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Philance, Inc.'])[1]/following::div[3]").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ frontend-test/howitworks1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+	options.add_argument('--headless')
+	options.add_argument('--disable-gpu')
+	options.add_argument('--window-size=1500,1000')
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:3000")
+        driver.find_element_by_xpath("//div[@id='root']/div/header/div/div[2]/ul/li[4]/a/div").click()
+        try: self.assertEqual("How It Works", driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div/div/div/h2").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("POST A PROJECT", driver.find_element_by_xpath("//button[@type='button']").text)
+        except AssertionError as e: self.verificationErrors.append(str(e)) 
+        try: self.assertEqual("WORK ON A PROJECT", driver.find_element_by_xpath("(//button[@type='button'])[2]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("//button[@type='button']").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/span/p/ul/li[5]").click()
+        try: self.assertEqual("Start the project and use PhiLance's project management tools to track tasks, milestones, resources and deliverables.", driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/span/p/ul/li[5]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[2]/div/span/p/ul/li[3]").click()
+        try: self.assertEqual("Browse projects using a variety of search criteria such as location, start date, budget, impact area, keywords, etc.", driver.find_element_by_xpath("//div[@id='root']/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[2]/div/span/p/ul/li[3]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
+
