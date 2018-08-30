d24e4251f1537ae45c52ee91c52ead553ede108c
manjot <manjot.simbacart@gmail.com>
2018-08-30 14:32:24 +0530

    Added New Configurations.

--- server-api/config/config.js
+++ server-api/config/config.js
@@ -5,6 +5,30 @@ var config={
             host:'localhost',
             port:':3001'
         }
-    }
+    },
+    testing:{
+        unsecure:{
+            protocol:'http://',
+            host:'philance.hopto.org',
+            port:':3001'
+        },
+        secure:{
+            protocol:'https://',
+            host:'philance.hopto.org',
+            port:':3001'
+        },
+    },
+    production:{
+        unsecure:{
+            protocol:'http://',
+            host:'philance.hopto.org',
+            port:':3001'
+        },
+        secure:{
+            protocol:'https://',
+            host:'philance.hopto.org',
+            port:':3001'
+        },
+    },
 }
 module.exports=config;
