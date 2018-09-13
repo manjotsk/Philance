235557b6f1edd38df28b242e7675e62c7b6db6cb
manjot <manjot.simbacart@gmail.com>
2018-09-13 19:38:13 +0530

    Changed Project Search API. Integrated it with UI

---
+++ my-app/src/philance/reducers/FindProjectReducer.js
@@ -0,0 +1,44 @@
+import {
+    FIND_PROJECT_TEXT_CHANGED,
+    FIND_PROJECT_LOCATION_CHANGED,
+    FIND_PROJECT_RESOURCE_CHANGED,
+    FIND_PROJECT_PROJECT_STATUS_CHANGED,
+    FIND_PROJECT_IMPACT_CATEGORIES_CHANGED,
+    FIND_PROJECT_COUNTRY_CHANGED,
+    FIND_PROJECT_KEYWORD_CHANGED,
+    FIND_PROJECTS_REQUEST_SUCCESS
+  } from '../actions/types'
+const INITIAL_STATE = {
+    tableData: [],
+    impactCategories: [],
+    yourLocation: "",
+    resourceType: "0",
+    projectStatus: "0",
+    distanceFromYou: "0",
+    keyword:'',
+    country:'',
+    textChanged:true
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case FIND_PROJECTS_REQUEST_SUCCESS:
+            return {...state, tableData:action.payload}
+        case FIND_PROJECT_TEXT_CHANGED:
+            return {...state, textChanged:action.payload}
+        case FIND_PROJECT_LOCATION_CHANGED:
+            return {...state, yourLocation:action.payload}
+        case FIND_PROJECT_COUNTRY_CHANGED:
+            return {...state, country:action.payload}
+        case FIND_PROJECT_IMPACT_CATEGORIES_CHANGED:
+            return {...state, impactCategories:action.payload}
+        case FIND_PROJECT_KEYWORD_CHANGED:
+            return {...state, keyword:action.payload}
+        case FIND_PROJECT_RESOURCE_CHANGED:
+            return {...state, resourceType:action.payload}
+        case FIND_PROJECT_PROJECT_STATUS_CHANGED:
+            return {...state, projectStatus:action.payload}
+        default:
+        return state
+    }
+}
