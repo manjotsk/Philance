0cd1bbc0954f19c394ff3ae65a581014e6a7e8cd
radhika <radhika.khindri@simbaquartz.com>
2018-09-19 15:41:33 +0530

    removed review candidate from pvt sidebar

--- my-app/src/philance/routes/pages.jsx
+++ my-app/src/philance/routes/pages.jsx
@@ -214,14 +214,6 @@ export const pvtSidebarRoutes = [
     path: "/",
     pathTo: "/home",
     name: "Home"
-  },
-  {
-    path: "/projectCandidateReview/:id",
-    name: "Project Candidate Review",
-    short: "Project Candidate Review",
-    mini: "PCR",
-    icon: MessageIcon,
-    component: CandidateReview
   }
 ];
 
